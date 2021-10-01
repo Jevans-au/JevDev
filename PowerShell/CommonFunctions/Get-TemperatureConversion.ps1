8404d82297e36440542071ff55db3f046a6ab9fd
Jevans-au <68683485+Jevans-au@users.noreply.github.com>
2021-10-01 15:32:47 +1000

    Fixed indenting

--- PowerShell/CommonFunctions/Get-TemperatureConversion.ps1
+++ PowerShell/CommonFunctions/Get-TemperatureConversion.ps1
@@ -29,37 +29,37 @@ function Get-TemperatureConversion {
         Author:     Jeff Evans (jevans.dev)
         Created:    2021/9/20
     #>
-        [CmdletBinding()]
-        param (
-            [Parameter(Mandatory=$true)]
-            $Temperature, # It grinds my gears to leave this without a type, but it's usually best to leave it to decide for itself
-            [Parameter(Mandatory=$true)]
-            [ValidateSet('celsius','fahrenheit','kelvin')]
-            [string]
-            $Unit
-        )
-        $OutputObject = [PSCustomObject]@{
-            celsius     = $null
-            fahrenheit  = $null
-            kelvin      = $null
-        }
-        # Just return the conversion for the other two by default
-        switch ($unit) {
-            'celsius' {
-                $OutputObject.celsius       = $Temperature
-                $OutputObject.fahrenheit    = ($Temperature * 1.8) + 32
-                $OutputObject.kelvin        = $Temperature + 273.15
-            }
-            'fahrenheit' {
-                $OutputObject.celsius       = ($Temperature - 32) / 1.8
-                $OutputObject.fahrenheit    = $Temperature
-                $OutputObject.kelvin        = $OutputObject.celsius + 273.15
-            }
-            'kelvin' {
-                $OutputObject.celsius       = $Temperature - 273.15
-                $OutputObject.fahrenheit    = ($OutputObject.celsius * 1.8) + 32
-                $OutputObject.kelvin        = $Temperature
-            }
-        }
-        Write-Output $OutputObject
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        $Temperature, # It grinds my gears to leave this without a type, but it's usually best to leave it to decide for itself
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('celsius','fahrenheit','kelvin')]
+        [string]
+        $Unit
+    )
+    $OutputObject = [PSCustomObject]@{
+        celsius     = $null
+        fahrenheit  = $null
+        kelvin      = $null
     }
+    # Just return the conversion for the other two by default
+    switch ($unit) {
+        'celsius' {
+            $OutputObject.celsius       = $Temperature
+            $OutputObject.fahrenheit    = ($Temperature * 1.8) + 32
+            $OutputObject.kelvin        = $Temperature + 273.15
+        }
+        'fahrenheit' {
+            $OutputObject.celsius       = ($Temperature - 32) / 1.8
+            $OutputObject.fahrenheit    = $Temperature
+            $OutputObject.kelvin        = $OutputObject.celsius + 273.15
+        }
+        'kelvin' {
+            $OutputObject.celsius       = $Temperature - 273.15
+            $OutputObject.fahrenheit    = ($OutputObject.celsius * 1.8) + 32
+            $OutputObject.kelvin        = $Temperature
+        }
+    }
+    Write-Output $OutputObject
+}
